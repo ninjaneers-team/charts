7f4daabcc546c6f45643d2f56eacbf7b00993dd7
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-01-20 14:52:52 +0100

    feat(update): change update

--- .ci/push.sh
+++ .ci/push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add remote https://${GH_TOKEN}@github.com/ninjaneers-team/charts.git > /dev/null 2>&1
+  git remote add remote https://${GITHUB_TOKEN}@github.com/ninjaneers-team/charts.git > /dev/null 2>&1
   git push --quiet remote master
 }
 
